9163c1f340f7459f63ff52afd1ace26187e45c58
Aaron Merryman <amerryma@asu.edu>
2022-02-04 10:13:01 -0700

    Better idempotency with functions, triggers, constraints, and rls pol… (#6)

--- tests/test_all.py
+++ tests/test_all.py
@@ -214,7 +214,14 @@ def test_postgres_objects():
     )
     assert (
         c.create_statement
-        == 'alter table "public"."films" add constraint "firstkey" PRIMARY KEY using index "firstkey";'
+        == """DO
+    $$
+        BEGIN
+            IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'firstkey') THEN
+                alter table "public"."films" add constraint "firstkey" PRIMARY KEY using index "firstkey";
+        END IF;
+    END
+$$;"""
     )
     c2 = deepcopy(c)
     assert c == c2
@@ -222,7 +229,14 @@ def test_postgres_objects():
     assert c != c2
     assert (
         c.create_statement
-        == 'alter table "public"."films" add constraint "firstkey" PRIMARY KEY (code);'
+        == """DO
+    $$
+        BEGIN
+            IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'firstkey') THEN
+                alter table "public"."films" add constraint "firstkey" PRIMARY KEY (code);
+        END IF;
+    END
+$$;"""
     )
     assert (
         c.drop_statement == 'alter table "public"."films" drop constraint "firstkey";'
@@ -402,7 +416,7 @@ def asserts_pg(i, has_timescale=False):
     assert f.create_statement == FDEF
     assert (
         f.drop_statement
-        == 'drop function if exists "public"."films_f"(d date, def_t text, def_d date);'
+        == 'drop function if exists "public"."films_f"(d date, def_t text, def_d date) cascade;'
     )
     assert f.comment == "films_f comment"
     assert f2.comment is None
@@ -420,7 +434,14 @@ def asserts_pg(i, has_timescale=False):
     cons = i.constraints['"public"."films"."firstkey"']
     assert (
         cons.create_statement
-        == 'alter table "public"."films" add constraint "firstkey" PRIMARY KEY using index "firstkey";'
+        == """DO
+    $$
+        BEGIN
+            IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'firstkey') THEN
+                alter table "public"."films" add constraint "firstkey" PRIMARY KEY using index "firstkey";
+        END IF;
+    END
+$$;"""
     )
 
     # tables
@@ -452,8 +473,6 @@ def asserts_pg(i, has_timescale=False):
     assert g.create_statement == 'grant select on table {} to "postgres";'.format(
         t_films
     )
-    print(g.drop_statement)
-
     assert g.drop_statement == """DO
 $$
     BEGIN
